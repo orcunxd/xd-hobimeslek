3e43431663d3201ec6b0f56057461ede7f82311c
orcunxd <130859127+orcunxd@users.noreply.github.com>
2023-04-22 18:54:26 +0300

    Yeni mesleklerin configleri eklendi
    Mandalin ve bal mesleğinin config kodları eklendi.

--- config.lua
+++ config.lua
@@ -10,7 +10,16 @@ Config.PortakalToplama = { -- Portakalların Çıkacağı Konumlar
         vector3(201.53, 6496.76, 31.47),
         vector3(208.52, 6509.29, 31.47)
 }
-
+Config.MandalinToplama = { -- Mandalinlerin Çıkacağı Konumlar
+        vector3(209.87, 6496.77, 31.43),
+        vector3(201.53, 6496.76, 31.47),
+        vector3(208.52, 6509.29, 31.47)
+}
+Config.BalToplama = { -- Balların Çıkacağı Konumlar
+        vector3(209.87, 6496.77, 31.43),
+        vector3(201.53, 6496.76, 31.47),
+        vector3(208.52, 6509.29, 31.47)
+}
 -- Mesleklerin Temelleri
 Config.MeslekislemeMiktar = 1 -- Mesleklerde malzeme işlemek için minimum gereken miktar
 Config.MeslekMin = 1  -- Mesleklerde toplarken minimum vereceği sayı
@@ -45,3 +54,29 @@ Config.PortakalSatisBlipAd = "Portakal Satış"
 Config.PortakalFiyat = 350 -- Portakal satış fiyatı
 Config.PortakalSatisNPC = vector4(291.8, 6524.24, 29.77, 96.1) --Satış NPC'sinin konumu
 Config.PortakalIslemeNPC = vector4(291.67, 6511.51, 29.77, 98.29) --İşleme NPC'sinin konumu
+
+--Mandalin Mesleği
+Config.xdMandalinIsleme = vector3(291.67, 6511.51, 29.77) -- Mandalin işleme kordinatı
+Config.xdMandalinSatis = vector3(291.8, 6524.24, 29.77) -- Mandalin satış kordinatı
+Config.MandalinToplamaBlip = vector3(201.53, 6496.76, 31.47) -- Mandalin toplama bölgesi için blip
+Config.MandalinToplamaBlipAd = "Mandalin Toplama" -- Mandalin toplama bölgesi için blip adı
+Config.MandalinIslemeBlip = vector3(291.8, 6524.24, 29.77) -- Mandalin işleme bölgesi için blip
+Config.MandalinIslemeBlipAd = "Mandalin İşleme" -- Mandalin işleme bölgesi için blip adı
+Config.MandalinSatisBlip = vector3(291.8, 6524.24, 29.77) -- Mandalin satış bölgesi için blip
+Config.MandalinSatisBlipAd = "Mandalin Satış"
+Config.MandalinFiyat = 350 -- Mandalin satış fiyatı
+Config.MandalinSatisNPC = vector4(291.8, 6524.24, 29.77, 96.1) --Satış NPC'sinin konumu
+Config.MandalinIslemeNPC = vector4(291.67, 6511.51, 29.77, 98.29) --İşleme NPC'sinin konumu
+
+--Bal Mesleği
+Config.xdBalIsleme = vector3(291.67, 6511.51, 29.77) -- Bal işleme kordinatı
+Config.xdBalSatis = vector3(291.8, 6524.24, 29.77) -- Bal satış kordinatı
+Config.BalToplamaBlip = vector3(201.53, 6496.76, 31.47) -- Bal toplama bölgesi için blip
+Config.BalToplamaBlipAd = "Bal Toplama" -- Bal toplama bölgesi için blip adı
+Config.BalIslemeBlip = vector3(291.8, 6524.24, 29.77) -- Bal işleme bölgesi için blip
+Config.BalIslemeBlipAd = "Bal İşleme" -- Bal işleme bölgesi için blip adı
+Config.BalSatisBlip = vector3(291.8, 6524.24, 29.77) -- Bal satış bölgesi için blip
+Config.BalSatisBlipAd = "Bal Satış"
+Config.BalFiyat = 350 -- Bal satış fiyatı
+Config.BalSatisNPC = vector4(291.8, 6524.24, 29.77, 96.1) --Satış NPC'sinin konumu
+Config.BalIslemeNPC = vector4(291.67, 6511.51, 29.77, 98.29) --İşleme NPC'sinin konumu
